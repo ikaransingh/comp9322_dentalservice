faabd96a74432807319051e67f4ccc9dda9704ef
Karan Inder Singh <karanindersingh@gmail.com>
2020-11-18 19:25:55 +1100

    update

--- wit/app/demo/__init__.py
+++ wit/app/demo/__init__.py
@@ -4,10 +4,11 @@ from __future__ import absolute_import
 from flask import Flask
 
 import v1
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__, static_folder='static')
+    CORS(app)
     app.register_blueprint(
         v1.bp,
         url_prefix='/v1')
